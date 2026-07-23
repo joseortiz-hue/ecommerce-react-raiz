b434ddcdfd4d3688ec455d04122a5f9b4cffa48e
José Miguel Ortiz Chamorro <josemiguelortizchamorro@gmail.com>
2026-07-22 21:40:22 -0500

    Corregida ruta de Card en Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import Card from '../components/Card'
+import Card from '../Components/Card'
 import toast from 'react-hot-toast'
 
 const Home = ({ cantidadCarrito, incrementarCarrito, carrito = [] }) => {
